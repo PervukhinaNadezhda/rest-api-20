89615e92dc98be431f3bc8fc463ac351b182f1f8
Nadezhda Pervukhina <Nadezhda-ov@mail.ru>
2023-08-07 17:37:02 +0300

    Разделила basePath baseURI

--- src/test/java/in/reqres/TestBase.java
+++ src/test/java/in/reqres/TestBase.java
@@ -2,10 +2,12 @@ package in.reqres;
 
 import org.junit.jupiter.api.BeforeAll;
 import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.basePath;
 
 public class TestBase {
     @BeforeAll
     static void baseURL() {
-        baseURI = "https://reqres.in/api";
+        baseURI = "https://reqres.in";
+        basePath = "/api";
     }
 }
